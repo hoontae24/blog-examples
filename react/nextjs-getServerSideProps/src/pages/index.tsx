6f654ce9cce84b6ad3a75d3456d6ed5614de9599
hoontae24 <kadr8941@gmail.com>
2022-06-01 17:06:20 +0900

    add examples

--- react/nextjs-getServerSideProps/src/pages/index.tsx
+++ react/nextjs-getServerSideProps/src/pages/index.tsx
@@ -1,12 +1,45 @@
+import { GetServerSideProps, NextPage } from "next";
+import Link from "next/link";
 import Navigation from "../components/Navigation";
 
-const MainPage = () => {
+interface Post {
+  id: number;
+  title: string;
+}
+
+interface MainPageProps {
+  name: string;
+  posts: Post[];
+}
+
+let count = 0;
+
+const MainPage: NextPage<MainPageProps> = (props) => {
+  const { name, posts } = props;
+  console.log("Logging from IndexPage Component", ++count);
   return (
     <div>
-      <h1>Hello Main!</h1>
+      <h1>Hello {name}!</h1>
+      <Link href="/about">about</Link>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
       <Navigation />
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<MainPageProps> = async () => {
+  console.log("Logging from getServerSideProps", ++count);
+  const posts: Post[] = await fetch("https://jsonplaceholder.typicode.com/posts").then((res) => res.json());
+  return {
+    props: {
+      name: "Next",
+      posts: posts,
+    },
+  };
 };
 
 export default MainPage;
